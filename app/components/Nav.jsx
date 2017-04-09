ffe16217d976be3e4c675227a4543a61b098f1b6
twasilonek <tomasz.wasilonek@gmail.com>
2017-04-09 12:02:40 +0000

    Styling the navigation

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,26 +1,60 @@
 var React = require("react");
 var {Link, IndexLink} = require('react-router');   // equals to Link = require('react-router').link; indexLink = require('react-router').indexLink
 
-var Nav = (props) => {
-    return (
-        <div>
-            <h2>Nav Components</h2>
-            {/* IndexLink is a component of react-router.
-                It should be used to the IndexRoute if it was set (see app.js -- IndexRoute is 'Weather')
-            */}
-            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-            
-            {/* Link is a component of react-router.
-                It allows for custom behavior, eg:
-                 - activeClassName : it will add the specified class once the 
-                                     specified route is in active state
-                 - activeStyle : it will add the specified style once the 
-                                  specified route is in active state
-            */}
-            <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-            <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-        </div>
-    );   
-};
+var Nav = React.createClass({
+    /* ====== REACT METHODS ====== */
+    
+    /* ====== USER METHODS ====== */
+    onSearch: function (e) {
+        e.preventDefault();
+        alert('Not yet wired up');
+    },
+    
+    /* ====== THE RENDER METHOD ====== */
+    render: function (props)  {
+        return (
+            <div className="top-bar">
+                {/* In React you should use 'className' instead of 'class' */ }
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">React Weather App</li>
+                        <li>
+                            {/* IndexLink is a component of react-router.
+                                It should be used to the IndexRoute if it was set (see app.js -- IndexRoute is 'Weather')
+                            */}
+                            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+                        </li>
+                        <li>
+                            {/* Link is a component of react-router.
+                                It allows for custom behavior, eg:
+                                 - activeClassName : it will add the specified class once the 
+                                                     specified route is in active state
+                                 - activeStyle : it will add the specified style once the 
+                                                  specified route is in active state
+                            */}
+                            <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+                        </li>
+                        <li>
+                            <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <form onSubmit={this.onSearch}>
+                        <ul className="menu">
+                            <li>
+                                <input type="search" placeholder="Search weather"/>
+                            </li>
+                            <li>
+                                <input type="submit" className="button" value="Get Weather"/>
+                            </li>
+                        </ul>
+                    </form>
+                </div>
+            </div>
+        );   
+    }
+});
+
 
 module.exports = Nav;
