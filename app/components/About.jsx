efbd3da79045ce54d0e7faf7e366897c67ecff23
twasilonek <tomasz.wasilonek@gmail.com>
2017-04-11 21:10:00 +0000

    Added content to About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -17,7 +17,20 @@ const React = require("react");
 */
 const About = (props) => {
     return ( 
-        <h3>About</h3>
+        <div>
+            <h1 className="text-center">About</h1>
+            <div className="media-object">
+                <div className="media-object-section">
+                    <div className="thumbnail">
+                      <img src="/images/react.svg" alt="React logo"/>
+                    </div>
+                </div>
+                <div className="media-object-section">
+                    <h4>React Weather App</h4>
+                    <p>This is an app for getting weather from the Open Weather Maps website. It is based on the <a href="https://www.udemy.com/the-complete-react-web-app-developer-course/">Complete React Web App Developer Course</a>, which I highly recommend for learning React.</p>
+                </div>
+            </div>
+        </div>
     );
 }
 
