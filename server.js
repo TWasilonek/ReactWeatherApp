d8e0e276569f2230d9ee230f55ea1367e4055971
twasilonek <tomasz.wasilonek@gmail.com>
2017-04-13 20:14:09 +0000

    Added node-geocoder, and migrated getting weather data to the server

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require("express");
 /*
     axios is a 3rd party library 
@@ -5,6 +7,7 @@ const express = require("express");
         * https://github.com/mzabriskie/axios
 */
 const axios = require("axios");
+const NodeGeocoder = require('node-geocoder');
 
 // create app
 const app = express();
@@ -14,10 +17,17 @@ const app = express();
 const hostname = process.env.IP;
 const port = process.env.PORT;
 
-// DarkSky config
+// NodeGeocoder config
+const geocoderOptions = {
+  provider: 'google',
+};
+const geocoder = NodeGeocoder(geocoderOptions);
+
+// Dark Sky config
 const darkSky = {
-    API_KEY : process.env.DARK_SKY_API_KEY,
-    URL     : `https://api.darksky.net/forecast/${darkSky.API_KEY}`
+    API_KEY : process.env.DARK_SKY_API_KEY,  // this is a private key, you can create your own by signing up to Dark Sky - https://darksky.net
+    URL     : 'https://api.darksky.net/forecast/',
+    EXCLUDES: '?exclude=minutely,hourly,daily,alerts,flags'
 };
 
 
@@ -38,17 +48,32 @@ app.use(express.static('public'));
 
 /* ----- ROUTES -----  */ 
 app.get('/weather/:location', (req, res) => {
+    function fetchWeatherForecast (geoData) {
+        // gather location data and set to Warsaw AS A FALLBACK.
+        // This fallback should be gone when ERROR HANDLING will be implemented
+        let latitude = geoData[0].latitude || '52.2296756';
+        let longitude =  geoData[0].longitude || '21.0122287';
+        let darkSkyUrl = darkSky.URL + darkSky.API_KEY + '/' + latitude + ',' + longitude + darkSky.EXCLUDES;
+        // console.log('geoData: ', geoData);
+        // console.log('lat: ', latitude);
+        // console.log('lon: ', longitude);
+        // console.log('darkSkyUrl: ', darkSkyUrl);
+        
+        return axios.get( darkSkyUrl );
+    }
+    function serveWeatherForecast (weather) {
+        // console.log('DarkSky: ', weather.data);
+        res.send(weather.data);
+    }
     
-    
-    
-  axios.get( darkSky.URL + req.params.location )
-    .then( (weather) => {
-        // Respond to the express call
-        res.json(weather);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+  geocoder.geocode(req.params.location)
+  .then( fetchWeatherForecast )
+  .then( serveWeatherForecast )
+  .catch(function(err) {
+    console.log('ERROR ', err)
+    // ADD ERROR HANDLING!
+  });
+  
 });
 
 
